2cccdb2d940d09b18f5d161ced95ba3c03a121ba
roos <rafael.borges@gft.com>
2020-12-01 12:04:31 -0300

    Add product and client repository

---
+++ src/main/java/com/gft/productapi/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.gft.productapi.repository;
+
+import com.gft.productapi.entity.Cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    
+}
