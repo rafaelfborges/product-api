08567a2884d746764c72e1ff69b220e4f0daa1d4
roos <rafael.borges@gft.com>
2020-12-04 09:29:19 -0300

    Add swagger annotations tags for company

--- src/main/java/com/gft/productapi/resource/EmpresaResouce.java
+++ src/main/java/com/gft/productapi/resource/EmpresaResouce.java
@@ -11,7 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+
 @RestController
+@Api(tags = "Empresas")
 @RequestMapping("/api/empresas")
 public class EmpresaResouce {
     
