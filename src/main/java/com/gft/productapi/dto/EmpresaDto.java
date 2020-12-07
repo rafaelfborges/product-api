ce4d640b02cb299c682fb1e02ed25e4792d0aafa
roos <rafael.borges@gft.com>
2020-12-07 09:44:10 -0300

    Remove lombok constructors in dto's

--- src/main/java/com/gft/productapi/dto/EmpresaDto.java
+++ src/main/java/com/gft/productapi/dto/EmpresaDto.java
@@ -1,13 +1,9 @@
 package com.gft.productapi.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class EmpresaDto {
     private Long id;
     private String nome;
