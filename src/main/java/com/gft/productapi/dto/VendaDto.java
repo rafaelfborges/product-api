831cab96049de97406095d05a1216e5745ffb730
roos <rafael.borges@gft.com>
2020-12-08 16:43:08 -0300

    Add sale dto and mapper

---
+++ src/main/java/com/gft/productapi/dto/VendaDto.java
@@ -0,0 +1,27 @@
+package com.gft.productapi.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.gft.productapi.entity.Produto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class VendaDto {
+    private Long id;
+    private EmpresaDto empresa;
+    private ClienteDto cliente;
+
+    @JsonIgnoreProperties("empresa")
+    private Set<Produto> produtos;
+
+    private BigDecimal totalCompra;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataCompra;
+}
---
+++ src/main/java/com/gft/productapi/mapper/VendaMapper.java
@@ -0,0 +1,23 @@
+package com.gft.productapi.mapper;
+
+import java.util.List;
+
+import com.gft.productapi.dto.VendaDto;
+import com.gft.productapi.entity.Venda;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface VendaMapper {
+    
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "empresa", target = "empresa")
+    @Mapping(source = "cliente", target = "cliente")
+    @Mapping(source = "produtos", target = "produtos")
+    @Mapping(source = "totalCompra", target = "totalCompra")
+    @Mapping(source = "dataCompra", target = "dataCompra", dateFormat = "dd/MM/yyyy")
+    VendaDto map(Venda venda);
+
+    List<VendaDto> map(List<Venda> vendas);
+}
