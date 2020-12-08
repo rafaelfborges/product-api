65566e7a359c9064756b64f9d3c595e987caf32b
roos <rafael.borges@gft.com>
2020-12-08 16:45:23 -0300

    Add the @Repository annotation in the interface

--- src/main/java/com/gft/productapi/repository/VendaRepository.java
+++ src/main/java/com/gft/productapi/repository/VendaRepository.java
@@ -3,7 +3,9 @@ package com.gft.productapi.repository;
 import com.gft.productapi.entity.Venda;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VendaRepository extends JpaRepository<Venda, Long> {
     
 }
