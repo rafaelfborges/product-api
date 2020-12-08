0ef50e38bbc83f33e57dc72cf9bcd31b165f8046
roos <rafael.borges@gft.com>
2020-12-08 16:42:44 -0300

    Some fix and changes in entity sale

--- src/main/java/com/gft/productapi/entity/Venda.java
+++ src/main/java/com/gft/productapi/entity/Venda.java
@@ -2,23 +2,25 @@ package com.gft.productapi.entity;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 @Data
 @Table
@@ -30,21 +32,22 @@ public class Venda {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @ManyToOne
-    @ToString.Exclude
     private Empresa empresa;
 
+    @NotNull
     @ManyToOne
-    @ToString.Exclude
     private Cliente cliente;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    private List<Produto> produtos;
+    @JsonIgnoreProperties("empresa")
+    @OneToMany(orphanRemoval = true, fetch = FetchType.EAGER)
+    private Set<Produto> produtos = new HashSet<>();
 
     @Column(name = "total_compra")
     private BigDecimal totalCompra;
 
     @Column(name = "data_compra")
-    @JsonFormat(pattern = "dd-MM-yyyy")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCompra;
 }
