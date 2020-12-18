a1f9bc04b55e3d0b6ba64988aeff06521e92ba93
Rafael Borges <rafaelfborges@gmail.com>
2020-12-18 09:52:25 -0300

    Update tables names on migrations

--- src/main/resources/db/migration/V02__criar_e_registrar_fornecedores.sql
+++ src/main/resources/db/migration/V02__criar_e_registrar_fornecedores.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `fornecedor` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `fornecedores` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `cnpj` varchar(255) DEFAULT NULL,
   `nome` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_rihyjxxhmgutledr1hdl5gpi8` (`cnpj`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `fornecedor` VALUES (1,'15.436.940/0001-03','Amazon'),(2,'33.014.556/0001-96','Americanas'),(3,'00.623.904/0001-73','Apple'),(4,'00.280.273/0002-18','Samsung');
+INSERT INTO `fornecedores` VALUES (1,'15.436.940/0001-03','Amazon'),(2,'33.014.556/0001-96','Americanas'),(3,'00.623.904/0001-73','Apple'),(4,'00.280.273/0002-18','Samsung');
--- src/main/resources/db/migration/V04__criar_tabela_vendas.sql
+++ src/main/resources/db/migration/V04__criar_tabela_vendas.sql
@@ -1,21 +1,21 @@
-CREATE TABLE `venda` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `vendas` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `data_compra` date NOT NULL,
   `total_compra` decimal(19,2) DEFAULT NULL,
-  `cliente_id` bigint(20) NOT NULL,
-  `fornecedor_id` bigint(20) NOT NULL,
+  `cliente_id` bigint NOT NULL,
+  `fornecedor_id` bigint NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK50murhuotq9h2dnxej317jjiy` (`cliente_id`),
   KEY `FK29grb6hh30perue3v1btueq6d` (`fornecedor_id`),
-  CONSTRAINT `FK29grb6hh30perue3v1btueq6d` FOREIGN KEY (`fornecedor_id`) REFERENCES `fornecedor` (`id`),
-  CONSTRAINT `FK50murhuotq9h2dnxej317jjiy` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`)
+  CONSTRAINT `FK29grb6hh30perue3v1btueq6d` FOREIGN KEY (`fornecedor_id`) REFERENCES `fornecedores` (`id`),
+  CONSTRAINT `FK50murhuotq9h2dnxej317jjiy` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `venda_produtos` (
-  `venda_id` bigint(20) NOT NULL,
-  `produtos_id` bigint(20) NOT NULL,
+CREATE TABLE `venda_produto` (
+  `venda_id` bigint NOT NULL,
+  `produtos_id` bigint NOT NULL,
   PRIMARY KEY (`venda_id`,`produtos_id`),
   UNIQUE KEY `UK_skdxfwlcsg1cxw7eddnkidf5g` (`produtos_id`),
-  CONSTRAINT `FK66sx48snxj3x5us4avomrfkli` FOREIGN KEY (`venda_id`) REFERENCES `venda` (`id`),
-  CONSTRAINT `FKaf1qmopp34f98prurcav28w7i` FOREIGN KEY (`produtos_id`) REFERENCES `produto` (`id`)
+  CONSTRAINT `FK66sx48snxj3x5us4avomrfkli` FOREIGN KEY (`venda_id`) REFERENCES `vendas` (`id`),
+  CONSTRAINT `FKaf1qmopp34f98prurcav28w7i` FOREIGN KEY (`produtos_id`) REFERENCES `produtos` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
