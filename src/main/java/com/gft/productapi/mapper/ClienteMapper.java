5de837c3043a27f96a604c690987325fbbe6ee7f
Rafael Borges <rafaelfborges@gmail.com>
2020-12-19 14:14:50 -0300

    Change mapper to map request and response

--- src/main/java/com/gft/productapi/mapper/ClienteMapper.java
+++ src/main/java/com/gft/productapi/mapper/ClienteMapper.java
@@ -2,7 +2,8 @@ package com.gft.productapi.mapper;
 
 import java.util.List;
 
-import com.gft.productapi.dto.ClienteDto;
+import com.gft.productapi.dto.request.ClienteRequestDto;
+import com.gft.productapi.dto.response.ClienteResponseDto;
 import com.gft.productapi.entity.Cliente;
 
 import org.mapstruct.Mapper;
@@ -10,13 +11,19 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ClienteMapper {
-    
+
+    @Mapping(source = "nome", target = "nome")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "senha", target = "senha")
+    @Mapping(source = "documento", target = "documento")
+    Cliente mapRequest(ClienteRequestDto clienteDto);
+    List<Cliente> mapRequest(List<ClienteRequestDto> clientes);
+
     @Mapping(source = "id", target = "id")
     @Mapping(source = "nome", target = "nome")
     @Mapping(source = "email", target = "email")
     @Mapping(source = "documento", target = "documento")
     @Mapping(source = "dataCadastro", target = "dataCadastro", dateFormat = "dd/MM/yyyy")
-    ClienteDto map(Cliente cliente);
-
-    List<ClienteDto> map(List<Cliente> cliente);
+    ClienteResponseDto mapResponse(Cliente cliente);   
+    List<ClienteResponseDto> mapResponse(List<Cliente> clientes);
 }
--- src/main/java/com/gft/productapi/mapper/FornecedorMapper.java
+++ src/main/java/com/gft/productapi/mapper/FornecedorMapper.java
@@ -1,14 +1,26 @@
 package com.gft.productapi.mapper;
 
-import com.gft.productapi.dto.FornecedorDto;
+import java.util.List;
+
+import com.gft.productapi.dto.request.FornecedorRequestDto;
+import com.gft.productapi.dto.response.FornecedorResponseDto;
 import com.gft.productapi.entity.Fornecedor;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface FornecedorMapper {
+    
+    @Mapping(source = "nome", target = "nome")
+    @Mapping(source = "cnpj", target = "cnpj")
+    Fornecedor mapRequest(FornecedorRequestDto fornecedor);
+    List<Fornecedor> mapRequest(List<FornecedorRequestDto> fornecedores);
 
     @Mapping(source = "id", target = "id")
     @Mapping(source = "nome", target = "nome")
-    FornecedorDto map(Fornecedor fornecedor);
+    @Mapping(source = "cnpj", target = "cnpj")
+    @Mapping(source = "produtos", target = "produtos")
+    FornecedorResponseDto mapResponse(Fornecedor fornecedor);
+    List<FornecedorResponseDto> mapResponse(List<Fornecedor> fornecedores);
 }
