bd3e931643434c915e0a9b27512e6079495844fe
Rafael Borges <rafaelfborges@gmail.com>
2020-12-19 14:21:12 -0300

    Remove unique key for product in venda_produto

--- src/main/resources/db/migration/V04__criar_tabela_vendas.sql
+++ src/main/resources/db/migration/V04__criar_tabela_vendas.sql
@@ -15,7 +15,7 @@ CREATE TABLE `venda_produto` (
   `venda_id` bigint NOT NULL,
   `produtos_id` bigint NOT NULL,
   PRIMARY KEY (`venda_id`,`produtos_id`),
-  UNIQUE KEY `UK_skdxfwlcsg1cxw7eddnkidf5g` (`produtos_id`),
+  KEY `UK_skdxfwlcsg1cxw7eddnkidf5g` (`produtos_id`),
   CONSTRAINT `FK66sx48snxj3x5us4avomrfkli` FOREIGN KEY (`venda_id`) REFERENCES `vendas` (`id`),
   CONSTRAINT `FKaf1qmopp34f98prurcav28w7i` FOREIGN KEY (`produtos_id`) REFERENCES `produtos` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
