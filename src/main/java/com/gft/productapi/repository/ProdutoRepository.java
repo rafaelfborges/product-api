66ce1a918617f2911e21886be0e217df88c828cc
Rafael Borges <rafaelfborges@gmail.com>
2020-12-19 14:15:56 -0300

    Remove unused interface methods

--- src/main/java/com/gft/productapi/repository/ProdutoRepository.java
+++ src/main/java/com/gft/productapi/repository/ProdutoRepository.java
@@ -1,16 +1,13 @@
 package com.gft.productapi.repository;
 
+import java.util.Optional;
+
 import com.gft.productapi.entity.Produto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    List<Produto> findAllByOrderByNomeAsc();
-    List<Produto> findAllByOrderByNomeDesc();
     Optional<Produto> findByNomeIgnoreCaseContaining(String nome);
 }
